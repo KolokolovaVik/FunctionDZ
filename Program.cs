89478958f42954ac0c5b845ddbb659c5f46fce6b
Viktoria Kolokolova <2645436@gmail.com>
2020-08-02 18:45:02 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fullName = new string[0];
+            string[] position = new string[0];
+            int serialNumber = 0;
+            
+            bool isOpen = true;
+            while (isOpen)
+            {
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine($"Ввыберите команду: \n 1) добавить досье." +
+                    $"\n 2) вывести всё досье\n " +
+                    $"3) удалить досье\n " +
+                    $"4) поиск по фамилии\n " +
+                    $"5) выход.");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                { 
+                    case 1:
+                        FillDossier(ref fullName, ref position);
+                        break;
+                    case 2:
+                        if (fullName.Length > 0 || position.Length > 0)
+                        {
+                            DrawDossier(fullName, position, ref serialNumber);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Досье не заполнено!");  
+                        }
+                        PressKey();
+                        break;
+                    case 3:
+                            RemoveDossier(ref fullName, ref position);
+                        break;
+                    case 4:
+                            SearchByName(fullName);
+                        break;
+                    case 5:
+                            Exit(ref isOpen);
+                        break;
+                }
+            }
+        }
+
+        static void FillDossier(ref string[] fullName, ref string[] position)
+        {
+            ReadFullNameAndPosition(ref fullName);
+            ReadFullNameAndPosition(ref position);
+            PressKey();    
+        }
+
+        static void DrawDossier(string[] fullName, string[] position, ref int serialNumber)
+        {
+            serialNumber = 0;
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                if (serialNumber < fullName.Length)
+                {
+                    ++serialNumber;
+                }
+                Console.WriteLine($"{serialNumber}.{fullName[i]} - {position[i]} ");
+            }
+        }
+
+        static void RemoveDossier(ref string[] fullName, ref string[] position)
+        {
+            Console.Write("Введите номер досье которое хотите удалить: ");
+
+            string[] newFullName = new string[fullName.Length - 1];
+            string[] newPosition = new string[position.Length - 1];
+            int serialNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                for (int i = 0; i < serialNumber; i++)
+                {
+                    newFullName[i] = fullName[i];
+                    newPosition[i] = position[i];
+                }
+                for (int i = serialNumber + 1; i < fullName.Length; i++)
+                {
+                    newFullName[i - 1] = fullName[i];
+                    newPosition[i - 1] = position[i];
+                }
+                fullName = newFullName;
+                position = newPosition;
+
+                PressKey();
+        }
+
+        static void SearchByName(string[] fullName)
+        {
+            bool surnameIsFind = false;
+            Console.WriteLine("Введите фамилию, которую хотите найти: ");
+            string userInput = Console.ReadLine();
+           
+            for (int i = 0; i< fullName.Length; i++)
+            {
+
+                string fullNameWorker = fullName[i];
+                string[] splited = fullNameWorker.Split(' ');
+                string surname = splited[0];
+                if (userInput.ToLower() == surname.ToLower())
+                {
+                    Console.WriteLine($"Фамилия: {surname} - есть в досье!");  
+                    surnameIsFind = true;
+                }   
+            }
+            if (!surnameIsFind)
+            {
+                Console.WriteLine("Такой фамилии в досье нет!");
+            }
+
+            PressKey();
+        }
+
+        static void Exit(ref bool isOpen)
+        {
+            isOpen = false;
+            Console.WriteLine("До скорых встреч!");
+            PressKey();
+        }
+
+        static void ReadFullNameAndPosition(ref string[] dossierData)
+        {
+            string[] fullNameWorkerOrPosition = new string[dossierData.Length + 1];
+            Console.WriteLine("Введите Вашу Фамилию, имя и отчество: ");
+
+            for (int i = 0; i < dossierData.Length; i++)
+            {
+                fullNameWorkerOrPosition[i] = dossierData[i];
+            }
+            fullNameWorkerOrPosition[fullNameWorkerOrPosition.Length - 1] = Console.ReadLine();
+            dossierData = fullNameWorkerOrPosition;
+        }
+
+        static void PressKey()
+        {
+            Console.WriteLine("Для продолжения нажмите любую клавишу!");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
