133d666774dbc47a68e27c59e528858945137f21
Viktoria Kolokolova <2645436@gmail.com>
2020-08-06 16:01:47 +0300

    For checking DZ3

---
+++ ProgramDZ3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            ReadNumber(out number);
+        }
+
+        static void ReadNumber(out int number)
+        {
+            number = 0;
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.Write("Введите число: ");
+                string userInput = Console.ReadLine();
+                bool result = int.TryParse(userInput, out number);
+                if (result)
+                {
+                    isWork = false;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не число, попробуйте ещё раз!");
+                }
+            }   
+        }
+    }
+}
